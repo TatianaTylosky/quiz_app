90c157661a58fa6a541a40b8752ad37103ac1641
TatianaTylosky <tatianajtylosky@Tatianas-MacBook-Air.local>
2014-06-15 16:39:59 -0400

    Quiz app 2.0
    Added styling and some javascript!

--- main.js
+++ main.js
@@ -1,65 +1,80 @@
 $(document).ready(function() {
 
-	var web_dev;
-	var angular = 0;
-	var ruby = 0;
-	var python = 0;
-	var swift;
+	$("#button").on("click", function() {
+
+		submitted();
+		window.event.returnValue = false;
+		document.location.href = 'results.html';
+		console.log("test 2");
+
+	});
 
 
-	if (experience = "none") {
-		web_dev = true;
-	}
+	function submitted() {
+		console.log("test 1");
+		var web_dev = false;
+		var angular = 0;
+		var ruby = 0;
+		var python = 0;
+		var swift = false;
 
-	else if (experience = "some") {
-		web_dev = true;
-	}
 
-	else if (experience = "frontend") {
-		angular++;
-		web_dev = false;
-	}
+		if (document.getElementById('none').checked) {
+			web_dev = true;
+		}
 
-	else if (experience = "backend") {
-		angular++;
-		web_dev = false;
-	}
+		else if (experience = "some") {
+			web_dev = true;
+		}
 
-	else {
+		else if (experience = "frontend") {
+			angular++;
+			web_dev = false;
+		}
 
+		else if (experience = "backend") {
+			angular++;
+			web_dev = false;
+		}
+
+		else {
+
+		};
+
+
+		if (learning_style = "ruby"){
+			ruby++;
+		}
+
+		else if (learning_style = "python") {
+			python++;
+		}
+
+		else {
+
+		};
+
+
+		if (goals = "swift") {
+			swift = true;
+		}
+
+		else if (goals = "web_dev") {
+			web_dev = true;
+		}
+
+		else if (goals = "ruby") {
+			ruby++;
+		}
+
+		else if (goals = "python") {
+			python++;
+		}
+
+		else if (goals = "angular") {
+			angular++;
+		}
+
+		console.log(web_dev);
 	};
-
-
-	if (learning_style = "ruby"){
-		ruby++;
-	}
-
-	else if (learning_style = "python") {
-		python++;
-	}
-
-	else {
-
-	};
-
-
-	if (goals = "swift") {
-		swift = true;
-	}
-
-	else if (goals = "web_dev") {
-		web_dev = true;
-	}
-
-	else if (goals = "ruby") {
-		ruby++;
-	}
-
-	else if (goals = "python") {
-		python++;
-	}
-
-	else if (goals = "angular") {
-		angular++;
-	}
 });
