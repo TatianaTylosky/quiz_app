222c423d22d976662672f41c3acd7f9e8ea77755
TatianaTylosky <tatianajtylosky@Tatianas-MacBook-Air.local>
2014-06-15 15:05:13 -0400

    javascript file with pseudo code

---
+++ main.js
@@ -0,0 +1,65 @@
+$(document).ready(function() {
+
+	var web_dev;
+	var angular = 0;
+	var ruby = 0;
+	var python = 0;
+	var swift;
+
+
+	if (experience = "none") {
+		web_dev = true;
+	}
+
+	else if (experience = "some") {
+		web_dev = true;
+	}
+
+	else if (experience = "frontend") {
+		angular++;
+		web_dev = false;
+	}
+
+	else if (experience = "backend") {
+		angular++;
+		web_dev = false;
+	}
+
+	else {
+
+	};
+
+
+	if (learning_style = "ruby"){
+		ruby++;
+	}
+
+	else if (learning_style = "python") {
+		python++;
+	}
+
+	else {
+
+	};
+
+
+	if (goals = "swift") {
+		swift = true;
+	}
+
+	else if (goals = "web_dev") {
+		web_dev = true;
+	}
+
+	else if (goals = "ruby") {
+		ruby++;
+	}
+
+	else if (goals = "python") {
+		python++;
+	}
+
+	else if (goals = "angular") {
+		angular++;
+	}
+});
